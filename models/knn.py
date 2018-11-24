05f2d1094ce22ec453ba85258084038ad357db4a
Yilun Guan <zoom.aaron@gmail.com>
2018-11-24 23:35:05 +0000

    Fix bug in knn model

--- models/knn.py
+++ models/knn.py
@@ -3,12 +3,6 @@ import numpy as np
 
 from mlpipe import Model
 
-#Train the model using the training sets
-knn.fit(X_train, y_train)
-
-#Predict the response for test dataset
-y_pred = knn.predict(X_test)
-
 
 class KNNModel(Model):
 
@@ -21,14 +15,15 @@ class KNNModel(Model):
         self.knn = KNeighborsClassifier(n_neighbors=7)
 
     def train(self, data, labels, metadata):
-        corrLive = metadata['corrLive']
-        rmsLive = metadata['rmsLive']
-        kurtLive = metadata['kurtLive']
-        skewLive = metadata['skewLive']
-        normLive = metadata['normLive']
+        corrLive = metadata['corrLive'][:,None]
+        rmsLive = metadata['rmsLive'][:,None]
+        kurtLive = metadata['kurtLive'][:,None]
+        skewLive = metadata['skewLive'][:,None]
+        normLive = metadata['normLive'][:,None]
         features = np.hstack([corrLive, rmsLive, kurtLive, skewLive, normLive])
+
         self.knn.fit(features, labels)
-        prediction = knn.predict(features)
+        prediction = self.knn.predict(features)
         return prediction
     
     def test(self, data, labels, metadata):
